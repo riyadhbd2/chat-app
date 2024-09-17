43e407491077373e9ff11d0c33d2ec21d7cce003
riyadhbd2 <riyadhbd2@gmail.com>
2024-09-17 16:40:48 +0200

    profile update page UI done

--- src/components/LeftSideBar.jsx
+++ src/components/LeftSideBar.jsx
@@ -1,18 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import assets from "../assets/assets";
 
 const LeftSideBar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const handleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <div className="bg-[#001030] text-white h-[75vh]">
       <div className="p-5">
         <div className="flex justify-between items-center">
           <img className="w-40" src={assets.logo} alt="" />
-          <div className="">
+          <div className="flex justify-between w-5 items-center">
             <img
+              onClick={handleMenu}
               className="w-12 opacity-60 cursor-pointer ..."
               src={assets.menu_icon}
               alt=""
             />
+            {showMenu && (
+              <div className="relative top-[100px] right-20  p-5 w-[200px] rounded-sm bg-white text-black">
+                <p className="cursor-pointer text-[10px]">Edit Profile</p>
+                <hr className="border-[1px]" />
+                <p className="cursor-pointer text-[10px]">Log Out</p>
+              </div>
+            )}
           </div>
         </div>
         <div className="bg-[#002670] flex items-center gap-2 px-2 py-3">
@@ -30,7 +44,10 @@ const LeftSideBar = () => {
         {Array(12)
           .fill("")
           .map((item, index) => (
-            <div key={index} className="flex items-center gap-2 px-3 py-5 hover:bg-[#077EFF]">
+            <div
+              key={index}
+              className="flex items-center gap-2 px-3 py-5 hover:bg-[#077EFF]"
+            >
               <img
                 className="w-9 aspect-square rounded-[50%]"
                 src={assets.profile_img}
--- src/pages/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import assets from '../assets/assets'
 
 const ProfileUpdate = () => {
+
+    const [image, setImage] = useState(false);
+
   return (
-    <div>ProfileUpdate</div>
+    <div className='min-h-[100vh] bg-[url(background.png)] bg-cover flex items-center justify-center'>
+        <div className='bg-white flex items-center justify-evenly min-w-[700px] rounded-lg'>
+            <form className='flex flex-col gap-5 p-10'>
+                <h3 className='font-medium'>Profile Details</h3>
+                <label className='flex items-center gap-2 text-gray-400' htmlFor="avatar">
+                    <input onChange={(e)=>setImage(e.target.files[0])} type="file" id='avatar' accept='.png, .jpg, .jpeg' hidden />
+                    <img className='w-12 aspect-square rounded-full' src={image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
+                    upload profile image
+                </label>
+                <input className='border border-gray-300 p-2 min-w-72 outline-blue-500' type="text" name="" id="" placeholder='Your name' required/>
+                <textarea className='border border-gray-300' name="" id="" cols="30" rows="10" placeholder='Write profile bio' required></textarea>
+                <button className='text-white bg-blue-400 p-2 text-sm cursor-pointer' type='submit'>Save</button>
+            </form>
+            <img className='max-w-40 aspect-square mx-20px my-auto rounded-full' src={image ? URL.createObjectURL(image) : assets.logo_icon} alt="" />
+        </div>
+    </div>
   )
 }
 
